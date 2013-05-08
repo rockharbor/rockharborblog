718ce225e7ac4da87b3c34d0ebebb507eb40ad69
Jeremy Harris <jharris@rockharbor.org>
2013-05-08 10:26:37 -0700

    Added footer

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
+<?php global $theme; ?>
 		</div>
+		<footer>
+			The RH <?php echo $theme->info('name'); ?> blog is part of the <a href="http://rockharbor.org"><strong>ROCK</strong>HARBOR</a> network.
+		</footer>
 	</div>
 <?php wp_footer(); ?>
 </body>
