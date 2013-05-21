649b7b5c524abdd284906b533d2c893d8fa988a5
Jeremy Harris <jharris@rockharbor.org>
2013-05-21 10:53:02 -0700

    Added staff post type to blogs

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function _include_theme() {
 	global $theme;
 	require_once 'libs/blog_theme.php';
 	$theme = new BlogTheme();
+	$theme->supports('staff', true);
 	$theme->init();
 }
 add_action('after_setup_theme', '_include_theme');
